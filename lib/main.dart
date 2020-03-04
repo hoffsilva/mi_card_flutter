4e0ee2e61250183a769d30e1c4efb19fdeda5f18
Hoff Silva <hoff.henry@gmail.com>
2020-03-03 21:52:25 -0300

    Finished study app

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,79 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
         body: SafeArea(
-          child: Column(
-            children: <Widget>[
-              CircleAvatar(
-                radius: 50,
-                backgroundColor: Colors.red
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            CircleAvatar(
+              radius: 50,
+              backgroundImage: NetworkImage(
+                  'https://avatars0.githubusercontent.com/u/11273459?s=460&v=4'),
+              backgroundColor: Colors.transparent,
+            ),
+            Text('Hoff Silva',
+                style: TextStyle(
+                    fontSize: 40,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                    fontFamily: 'Pacifico')),
+            Text('iOS Developer',
+                style: TextStyle(
+                    color: Colors.teal.shade100,
+                    fontSize: 20,
+                    letterSpacing: 2.5,
+                    fontWeight: FontWeight.bold,
+                    fontFamily: 'Source Sans Pro')
+              ),
+            SizedBox(
+              height: 20,
+              width: 150,
+              child: Divider(
+                color: Colors.teal.shade100
+              ),
+            ),
+            Card(
+              color: Colors.white,
+              margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+              child: ListTile(
+                leading: Icon(Icons.phone, color: Colors.teal),
+                title: Text(
+                      '+5561991212700',
+                      style: TextStyle(
+                          color: Colors.teal.shade900,
+                          fontFamily: 'Source Sans Pro',
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold),
+                    )
               )
-            ],
-          )
-        ),
+            ),
+            Card(
+              color: Colors.white,
+              margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+              child: ListTile(
+                leading: Icon(Icons.email, color: Colors.teal),
+                title: Text(
+                    'hoff.henry@gmail.com',
+                    style: TextStyle(
+                        color: Colors.teal.shade900,
+                        fontFamily: 'Source Sans Pro',
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold),
+                  )
+              )
+            )
+          ],
+        )),
       ),
-    ),
-  );
+    );
+  }
 }
